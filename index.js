e86281145756119a83b5c5432c8b05377d9c5914
ggglllkkk <emohu@outlook.fr>
2025-03-20 10:24:04 +0100

    ca marche

--- index.js
+++ index.js
@@ -1,10 +1,104 @@
-var subjects=[["Maths", 7, [["DS", 4], ["Tests", 1, 5]]], ["Physique", 7, [["DS", 4], ["Tests", 1]]], ["Chimie", 4, [["DS", 4], ["DS", 1]]], ["Bio", 3, [["DS", 4], ["tests", 1]]], ["Info", 3, [["tests", 1]]], ["Anglais", 1.5, [["tests", 1]]], ["LV2", 1.5, [["tests", 1]]], ["Sport", 1.5, [["tests", 1]]], ["Eco", 1.5, [["tests", 1]]]];
+var subjects=[["Maths", 7, [["DS", 4], ["Tests", 1, 5]]], ["Physique", 7, [["DS", 4], ["Tests", 1]]], ["Chimie", 4, [["DS", 4], ["tests", 1]]], ["Bio", 3, [["DS", 4], ["tests", 1]]], ["Info", 3, [["tests", 1]]], ["Anglais", 1.5, [["tests", 1]]], ["LV2", 1.5, [["tests", 1]]], ["Sport", 1.5, [["tests", 1]]], ["Eco", 1.5, [["tests", 1]]]];
 var notes=[]
+var globalOutOf=20;
 
+// cleans the page
+function cleanPage(){
+    loadNotes(-1);
+    buildTable();
+}
 
 // calculs the current average
 function calculAverage(){
-    
+    var avg=0;
+    var coeffSum=0
+    var avgModified=false;
+
+    for (let k in notes){
+        var subjectAvg=0;
+        var subjectCoeffSum=0;
+        var subjectAvgModified=false;
+
+        for (let n in notes[k]){
+            var subSubjectAvg=0;
+            var subSubjectAvgModified = false;
+            var numberOfNotes = 0;
+
+            for (let i in notes[k][n]){
+                var currentNote = notes[k][n][i]
+                if(subjects[k][2][n].length==3){var noteOn = subjects[k][2][n][2];}
+                else{var noteOn = 20;}
+
+                // checks if the current note is a number and modifies the parametesr if the note is on a unusual number
+                var isNoteValue=true;
+                var isNoteOnUnusual = false;
+                for (let k in currentNote){isNoteValue &&="0123456789.,/".includes(currentNote[k]); isNoteOnUnusual ||="/".includes(currentNote[k]);}
+
+                // computes if there is a note
+                if(currentNote!="" && isNoteValue){
+                    if(isNoteOnUnusual){
+                        currentNote = currentNote.split("/");
+                        noteOn = currentNote[1];
+                        currentNote = currentNote[0];
+                    }
+                    normalizedNote = parseFloat(currentNote)*100/parseFloat(noteOn);
+                    subSubjectAvg+=normalizedNote;
+                    numberOfNotes++;
+                    subSubjectAvgModified=true;
+                }
+            }
+
+            // calculates the average of the subsubject
+            if(subSubjectAvgModified){
+                subSubjectAvg/=numberOfNotes;
+                subjectAvg+=subSubjectAvg*parseFloat(subjects[k][2][n][1]);
+                subjectCoeffSum+=parseFloat(subjects[k][2][n][1]);
+                subjectAvgModified=true;
+                console.log("subsub", subSubjectAvg);
+            }
+        }
+        
+        // calculates the average of the subject and modifies the subject average panel
+        var column=document.getElementById("tableDiv").childNodes[k];
+        var subjectAverageDivChildNodes = column.childNodes[column.childNodes.length-1].childNodes;
+        if(subjectAvgModified){
+            subjectAvg/=subjectCoeffSum;
+            avg+=subjectAvg*parseFloat(subjects[k][1]);
+            coeffSum+=parseFloat(subjects[k][1]);
+            avgModified=true;
+            console.log("sub", subjectAvg, subjectCoeffSum);
+            
+
+            subjectAverageDivChildNodes[0].innerHTML=(subjectAvg*globalOutOf/100).toFixed(2);
+            subjectAverageDivChildNodes[0].visibility="visible";
+            subjectAverageDivChildNodes[1].visibility="visible";
+        }
+        else{
+            subjectAverageDivChildNodes[0].visibility="collapse";
+            subjectAverageDivChildNodes[1].visibility="collapse";
+        }
+    }
+
+    console.log("avg", avg, coeffSum);
+
+    // calculates the average and modifies the average panel accordingly
+    var avgTextField = document.getElementById("average");
+    var outOfTextField = document.getElementById("outOf");
+    if(avgModified){
+        avg/=coeffSum;
+        avg/=100/globalOutOf;
+
+        console.log("moyenne", avg);
+        
+        avgTextField.innerHTML=avg.toFixed(2);
+        avgTextField.style.visibility = "visible";
+        outOfTextField.style.visibility = "visible";
+    }
+    else{
+        avgTextField.style.visibility = "collapse";
+        outOfTextField.style.visibility = "collapse";
+    }
+
 }
 
 
@@ -14,6 +108,7 @@ function loadNotes(givenNotes=-1){
         notes=givenNotes;
     }
     else{
+        notes=[];
         for (let k in subjects){
             notes.push([]);
             for (let n in subjects[k][2]){
@@ -21,6 +116,7 @@ function loadNotes(givenNotes=-1){
             }
         }
     }
+    localStorage.setItem("notes", JSON.stringify(notes));
 
     console.log("notes loaded", notes);
 }
@@ -36,41 +132,40 @@ function inputModified(element){
     // verifies if the new value is correct and updates the notes array
     var value = targetedInput.value.replace(",", ".");
     isValueNumber = true;
-    for(let k in value){if("0123456789.,/".includes(value[k])==false){isValueNumber=false;}}
-    if (isValueNumber) {notes[targetedSubject[0]][targetedSubject[1]][targetedSubject[2]]=value;}
-    else{notes[targetedSubject[0]][targetedSubject[1]][targetedSubject[2]]="";}
+    //for(let k in value){if("0123456789.,/".includes(value[k])==false){isValueNumber=false;}}
+    for(let k in value){isValueNumber &&="0123456789.,/".includes(value[k]);}
+    notes[targetedSubject[0]][targetedSubject[1]][targetedSubject[2]]=value;
 
-    // changes the color of the input if incorrect
-    if (!isValueNumber){targetedInput.style.backgroundColor = "red";}
-    else{targetedInput.style.backgroundColor = "transparent";}
-
+    // adds a line if there are no more left
     if (targetedSubject[2] == notes[targetedSubject[0]][targetedSubject[1]].length-1 && targetedSubject.value!=""){
-        for (let k in notes[targetedSubject[0]]){
-            notes[targetedSubject[0]][k].push("");
-        }
+        notes[targetedSubject[0]][targetedSubject[1]].push("");
+        console.log("newline ?!")
     }
 
-    // checks if it makes the current line empty
-    var isLineEmpty = true;
-    for(let k in notes[targetedSubject[0]]){
-        if(notes[targetedSubject[0]][k][targetedSubject[2]]!=""){isLineEmpty=false;}
+    // gets the last filled line
+    var lastFilledLine = -1;
+    i=notes[targetedSubject[0]][targetedSubject[1]].length-1;
+    while (lastFilledLine==-1 && i>=0){
+        if(notes[targetedSubject[0]][targetedSubject[1]][i]!=""){lastFilledLine=i;}
+        i--;
+        console.log(i, notes[targetedSubject[0]][targetedSubject[1]][i])
     }
 
-    //console.log(isLineEmpty);
+    console.log("LASTFILLEDLINE", lastFilledLine+1, notes[targetedSubject[0]][targetedSubject[1]].length-lastFilledLine-2);
+    console.log("csc", lastFilledLine, notes[targetedSubject[0]][targetedSubject[1]].length)
 
-    // deletes said line if empty
-    if (isLineEmpty){
-        for(let k in notes[targetedSubject[0]]){
-            notes[targetedSubject[0]][k].splice([targetedSubject[2]], 1);
-        }
-    }
-
+    // deletes all the other empty lines
+    notes[targetedSubject[0]][targetedSubject[1]].splice(lastFilledLine+1, notes[targetedSubject[0]][targetedSubject[1]].length-lastFilledLine-2);
+    
     // saves the changes to the notes
     localStorage.setItem("notes", JSON.stringify(notes));
 
-    calculAverage();
+    console.log(notes);
 
     buildTable();
+
+    // changes the color of the input if incorrect
+    if (!isValueNumber){document.getElementById(targetedInput.id).style.backgroundColor = "red";}
 }
 
 // builds the average calcul table
@@ -126,12 +221,28 @@ function buildTable(){
             }
         }
 
+        var subjectAverageDiv = document.createElement("div");
+        subjectAverageDiv.classList.add("subjectAverageDiv");
+
+        var subjectAvg=document.createElement("text");
+        subjectAvg.classList.add("subjectAverage");
+        subjectAverageDiv.appendChild(subjectAvg);
+
+        var subjectOutOf=document.createElement("text");
+        subjectOutOf.classList.add("subjectOutOf");
+        subjectOutOf.innerHTML = "/"+globalOutOf;
+        subjectAverageDiv.appendChild(subjectOutOf);
+
+        column.appendChild(subjectAverageDiv);
+
         // adds the column to the table section
         document.getElementById("tableDiv").appendChild(column);
     }
 
     // replaces the cursor where it should be
-    if(activeElementId!=""){document.getElementById(activeElementId).focus();}
+    if(activeElementId!="" && isNaN(activeElementId) && Array.from(document.querySelectorAll('[id]')).map(el => el.id).includes(activeElementId)){document.getElementById(activeElementId).focus();}
+
+    calculAverage();
 }
 
 // initializes the page
@@ -143,6 +254,8 @@ function initialize(){
     cssFile.rel="stylesheet";
     cssFile.type="text/css";
     document.getElementsByTagName("head")[0].appendChild(cssFile);
+
+    document.getElementById("cleanButton").addEventListener("click", cleanPage);
 
     loadNotes(JSON.parse(localStorage.getItem("notes")));
 
