d0caf313d1f6f58ea59970fc5184a1135dcf25a0
ggglllkkk <emohu@outlook.fr>
2025-03-29 11:03:35 +0100

    working notes order before button

--- index.js
+++ index.js
@@ -4,68 +4,135 @@ var notesOrder=[]
 var globalOutOf=20;
 var colorsCoeff = [[0.341328, -10.68, 64.6337, 254.862353], [-0.2791, 6.19454, -12.306, -1.4046463], [-0.1835, 6.657, -46.6007, 4.32]];
 var strongColors = false;
+var showAverageGraph = false;
 var isMobileBrowser = false;
 
 
-// makes the average chart
-function makeAverageChart(){
-    
-    // chart variables
-    var xValues=[]
-    var yValues=[]
-    var minValue=20;
-    var maxValue=0;
-    var currentNotesArray=[]
-
-    // initializes the current notes array
-    for (let k in subjects){
-        currentNotesArray.push([]);
-        for (let n in subjects[k][2]){
-            currentNotesArray[k].push([]);
-        }
-    }
-
-    // computes the chart values
+// called to place correctly the notes order
+function assignNoteOrder(){
     for (let k in notesOrder){
-        var currentNoteIndexes=notesOrder[k];
-        currentNotesArray[currentNoteIndexes[0]][currentNoteIndexes[1]].push(notes[currentNoteIndexes[0]][currentNoteIndexes[1]][currentNoteIndexes[2]]);
-        var currentNote=calculAverage(currentNotesArray, false);
-        currentNote=currentNote.toFixed(2);
-        xValues.push(parseInt(k));
-        yValues.push(parseFloat(currentNote));
+        var currentNoteIndex=notesOrder[k];
+        document.getElementById("idInput;"+currentNoteIndex[0]+";"+currentNoteIndex[1]+";"+currentNoteIndex[2]).parentNode.childNodes[1].value=parseInt(k)+1;
+    }
+}
 
-        if(currentNote<minValue){minValue=currentNote;}
-        if(currentNote>maxValue){maxValue=currentNote;}
+// called when the notes order is modified
+function orderModified(element){
+
+    // determines the modified field
+    var targetedInput = element.target;
+    var targetedSubject = targetedInput.parentNode.childNodes[0].id.replace("idInput;", "").split(";");
+    var targetedColumn = targetedInput.parentNode.parentNode;
+
+    // veririfes is the new value is correct
+    var value = targetedInput.value;
+    isValueNumber = true;
+    for(let k in value){isValueNumber &&="0123456789.,/".includes(value[k]);}
+
+    // checks the current note's order
+    var lastNoteOrder = -1;
+    for (let k in notesOrder){
+        var currentNote= notesOrder[k]
+        if(currentNote[0]==targetedSubject[0] && currentNote[1]==targetedSubject[1] && currentNote[2]==targetedSubject[2]){
+            lastNoteOrder=k;
+        }
     }
 
-    minValue=Math.floor(minValue);
-    maxValue=Math.floor(maxValue)+1;
-    if(minValue<0){minValue=0;}
-    if(maxValue>20){maxValue=20;}
+    console.log(value!="", lastNoteOrder)
 
-    console.log("xyvalues", xValues, yValues);
-    console.log(minValue, maxValue)
-
-    // plots the average chart
-    new Chart("averageChart", {
-        type: "line",
-        data: {
-          labels: xValues,
-          datasets: [{
-            fill: false,
-            lineTension: 0,
-            backgroundColor: "rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
-            data: yValues
-          }]
-        },
-        options: {
-          legend: {display: false},
-          scales: {
-            yAxes: [{ticks: {min: minValue, max:maxValue}}],
-          }
+    // updates the notesOrder array
+    if(isValueNumber){
+        if(lastNoteOrder!=-1){notesOrder.splice(lastNoteOrder, 1);}
+        if(value!=""){
+            notesOrder.splice(value, 0, targetedSubject);
         }
-      });
+        else{
+            //notesOrder.splice(value+1, 1);
+        }
+    }
+
+    // saves the changes
+    localStorage.setItem("notesOrder", JSON.stringify(notesOrder));
+
+    console.log(notesOrder)
+    assignNoteOrder();
+    makeAverageGraph();
+}
+
+// makes the average chart
+function makeAverageGraph(){
+    
+    // hides or shows the chart
+    if(!showAverageGraph){
+        document.getElementById("averageGraph").style.visibility = "collapse";
+        var listOfNoteOrderInputs = document.getElementsByClassName("noteOrderInput")
+        for (let k=0; k<listOfNoteOrderInputs.length; k++){
+            listOfNoteOrderInputs[k].style.display = "none";
+        }
+    }
+    else{
+        document.getElementById("averageGraph").style.visibility = "visible";
+        var listOfNoteOrderInputs = document.getElementsByClassName("noteOrderInput")
+        for (let k=0; k<listOfNoteOrderInputs.length; k++){
+            listOfNoteOrderInputs[k].style.display = "initial";
+        }
+
+        // chart variables
+        var xValues=[]
+        var yValues=[]
+        var minValue=20;
+        var maxValue=0;
+        var currentNotesArray=[]
+
+        // initializes the current notes array
+        for (let k in subjects){
+            currentNotesArray.push([]);
+            for (let n in subjects[k][2]){
+                currentNotesArray[k].push([]);
+            }
+        }
+
+        // computes the chart values
+        for (let k in notesOrder){
+            var currentNoteIndexes=notesOrder[k];
+            currentNotesArray[currentNoteIndexes[0]][currentNoteIndexes[1]].push(notes[currentNoteIndexes[0]][currentNoteIndexes[1]][currentNoteIndexes[2]]);
+            var currentNote=calculAverage(currentNotesArray, false);
+            currentNote=currentNote.toFixed(2);
+            xValues.push(parseInt(k));
+            yValues.push(parseFloat(currentNote));
+
+            if(currentNote<minValue){minValue=currentNote;}
+            if(currentNote>maxValue){maxValue=currentNote;}
+        }
+
+        minValue=Math.floor(minValue);
+        maxValue=Math.floor(maxValue)+1;
+        if(minValue<0){minValue=0;}
+        if(maxValue>20){maxValue=20;}
+
+        console.log("xyvalues", xValues, yValues);
+
+        // plots the average chart
+        new Chart("averageGraph", {
+            type: "line",
+            data: {
+              labels: xValues,
+              datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                data: yValues
+              }]
+            },
+            options: {
+              legend: {display: false},
+              scales: {
+                yAxes: [{ticks: {min: minValue, max:maxValue}}],
+              }
+            }
+          });
+    }
 }
 
 
@@ -97,6 +164,12 @@ function calculAverageColor(note){
     }
 
     return "rgb("+colors[0]+","+colors[1]+","+colors[2]+")";
+}
+
+// changes the visibility of the graph
+function changeShowGraph(){
+    showAverageGraph = document.getElementById("showGraphButton").checked;
+    makeAverageGraph();
 }
 
 // changes the color strength
@@ -257,7 +330,7 @@ function loadNotes(givenNotes, givenNotesOrder){
     if (isNotesFormatCorrect){
         notes=givenNotes;
     }
-    else{
+    else{ // creates an adequate empty notes array
         notes=[];
         for (let k in subjects){
             notes.push([]);
@@ -278,38 +351,47 @@ function inputModified(element){
     // determines the modified field
     var targetedInput = element.target;
     var targetedSubject = targetedInput.id.replace("idInput;", "").split(";");
-    var targetedColumn = targetedInput.parentNode
+    var targetedColumn = targetedInput.parentNode.parentNode
 
     // verifies if the new value is correct and updates the notes array
     var value = targetedInput.value.replace(",", ".");
     isValueNumber = true;
-    //for(let k in value){if("0123456789.,/".includes(value[k])==false){isValueNumber=false;}}
     for(let k in value){isValueNumber &&="0123456789.,/".includes(value[k]);}
     notes[targetedSubject[0]][targetedSubject[1]][targetedSubject[2]]=value;
-    //notesOrder.push(targetedSubject);
 
     // adds a line if there are no more left
     if (targetedSubject[2] == notes[targetedSubject[0]][targetedSubject[1]].length-1 && targetedSubject.value!=""){
         notes[targetedSubject[0]][targetedSubject[1]].push("");
         //console.log("newline ?!")
 
+        // creates the input box container
+        var inputBoxDiv = document.createElement("div");
+        inputBoxDiv.style.gridColumn = parseInt(targetedSubject[1])+1;
+        inputBoxDiv.style.gridRow = 4+parseInt(targetedSubject[2]);
+
         // creates the input boxes and sets it up
         var inputBox = document.createElement("input");
         inputBox.classList.add("inputBox");
-        inputBox.style.gridColumn = parseInt(targetedSubject[1])+1;
-        inputBox.style.gridRow = 4+parseInt(targetedSubject[2]);
         inputBox.type = "text";
         inputBox.addEventListener("input", (element) => inputModified(element));
         inputBox.addEventListener("change", (element) => inputModified(element));
         inputBox.id = "idInput;"+targetedSubject[0]+";"+targetedSubject[1]+";"+parseInt(parseInt(targetedSubject[2])+1);
+
+        // creates the note order input
+        var noteOrderInput = document.createElement("input");
+        noteOrderInput.classList.add("noteOrderInput");
+        noteOrderInput.type="text";
+
+        inputBoxDiv.appendChild(inputBox);
+        inputBoxDiv.appendChild(noteOrderInput);
 
         // creates the input box's placeholder
         if(subjects[targetedSubject[0]][2][targetedSubject[1]].length==3){inputBox.placeholder = "/"+subjects[targetedSubject[0]][2][targetedSubject[1]][2];}
         else{inputBox.placeholder = "/20";}
 
         // adds the input to its column
-        if(targetedSubject[1]==subjects[targetedSubject[0]][2].length-1){targetedColumn.insertBefore(inputBox, targetedColumn.childNodes[targetedColumn.childNodes.length-1]);}
-        else{targetedColumn.insertBefore(inputBox, document.getElementById("subSubjectId;"+targetedSubject[0]+";"+parseInt(parseInt(targetedSubject[1])+1)));}
+        if(targetedSubject[1]==subjects[targetedSubject[0]][2].length-1){targetedColumn.insertBefore(inputBoxDiv, targetedColumn.childNodes[targetedColumn.childNodes.length-1]);}
+        else{targetedColumn.insertBefore(inputBoxDiv, document.getElementById("subSubjectId;"+targetedSubject[0]+";"+parseInt(parseInt(targetedSubject[1])+1)));}
     
     }
     
@@ -328,7 +410,7 @@ function inputModified(element){
     // deletes all the other empty lines
     for(let i=lastFilledLine+2; i<notes[targetedSubject[0]][targetedSubject[1]].length; i++){
         //console.log("FUUUUCK", "idInput;"+targetedSubject[0]+";"+targetedSubject[1]+";"+i)
-        document.getElementById("idInput;"+targetedSubject[0]+";"+targetedSubject[1]+";"+i).remove();
+        document.getElementById("idInput;"+targetedSubject[0]+";"+targetedSubject[1]+";"+i).parentElement.remove();
     }
     notes[targetedSubject[0]][targetedSubject[1]].splice(lastFilledLine+1, notes[targetedSubject[0]][targetedSubject[1]].length-lastFilledLine-2);
 
@@ -340,7 +422,7 @@ function inputModified(element){
 
     //buildTable();
     calculAverage();
-    makeAverageChart();
+    makeAverageGraph();
 
     // changes the color of the input if incorrect
     if (!isValueNumber){document.getElementById(targetedInput.id).style.backgroundColor = "red";}
@@ -383,23 +465,34 @@ function buildTable(){
             column.appendChild(subSubjectHeader);
 
             for (let i in notes[k][n]){
+                // create the input box container
+                var inputBoxDiv = document.createElement("div");
+                inputBoxDiv.classList.add("inputBoxDiv");
+                inputBoxDiv.style.gridColumn = parseInt(n)+1;
+                inputBoxDiv.style.gridRow = 3+parseInt(i);
+
                 // creates the input boxes and sets it up
                 var inputBox = document.createElement("input");
                 inputBox.classList.add("inputBox");
-                inputBox.style.gridColumn = parseInt(n)+1;
-                inputBox.style.gridRow = 3+parseInt(i);
                 inputBox.type = "text";
                 inputBox.addEventListener("input", (element) => inputModified(element));
                 inputBox.addEventListener("change", (element) => inputModified(element));
                 inputBox.id = "idInput;"+k+";"+n+";"+i;
                 inputBox.value = notes[k][n][i];
-
-                // creates the input box's placeholder
                 if(subjects[k][2][n].length==3){inputBox.placeholder = "/"+subjects[k][2][n][2];}
                 else{inputBox.placeholder = "/20";}
 
+                // creates the note order input
+                var noteOrderInput = document.createElement("input");
+                noteOrderInput.classList.add("noteOrderInput");
+                noteOrderInput.type="text";
+                noteOrderInput.addEventListener("input", (element) => orderModified(element));
+                //noteOrderInput.addEventListener("change", (element) => orderModified(element));
+
                 // adds the input to its column
-                column.appendChild(inputBox);
+                inputBoxDiv.appendChild(inputBox);
+                inputBoxDiv.appendChild(noteOrderInput);
+                column.appendChild(inputBoxDiv);
             }
         }
 
@@ -425,7 +518,8 @@ function buildTable(){
     // replaces the cursor where it should be
     if(activeElementId!="" && isNaN(activeElementId) && Array.from(document.querySelectorAll('[id]')).map(el => el.id).includes(activeElementId)){document.getElementById(activeElementId).focus();}
 
-    makeAverageChart();
+    assignNoteOrder();
+    makeAverageGraph();
     calculAverage();
 }
 
@@ -434,6 +528,7 @@ function initialize(){
 
     // initializes some variables
     strongColors = document.getElementById("strongColorsButton").checked;
+    showAverageGraph = document.getElementById("showGraphButton").checked;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) isMobileBrowser = true;})(navigator.userAgent||navigator.vendor||window.opera);
 
     // loads the css main stylesheet
@@ -454,6 +549,7 @@ function initialize(){
     // makes the buttons work
     document.getElementById("cleanButton").addEventListener("click", cleanPage);
     document.getElementById("strongColorsButton").addEventListener("click", changeColorStrength);
+    document.getElementById("showGraphButton").addEventListener("click", changeShowGraph);
     document.getElementById("importDataButton").addEventListener("click", importData);
     document.getElementById("exportDataButton").addEventListener("click", exportData);
 
